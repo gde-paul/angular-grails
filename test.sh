a22537376a4637a037547b57a980f28727371a6a
Craig Burke <craig@craigburke.com>
2014-07-29 09:01:41 -0400

    Fixing selenium download location in test script

--- test.sh
+++ test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
-SELENIUM_VERSION="2.39.0"
+SELENIUM_VERSION="2.42.2"
+SELENIUM_FOLDER=${SELENIUM_VERSION%.*}
 
 function start_selenium {
 	sudo start xvfb
-	wget "http://selenium.googlecode.com/files/selenium-server-standalone-$SELENIUM_VERSION.jar" --quiet
+	wget "http://selenium-release.storage.googleapis.com/$SELENIUM_FOLDER/selenium-server-standalone-$SELENIUM_VERSION.jar" --quiet
 	java -jar "selenium-server-standalone.$SELENIUM_VERSION.jar" > /dev/null 2>&1 &
 }
 
