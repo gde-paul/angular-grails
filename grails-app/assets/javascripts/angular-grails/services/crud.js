261f8007f8f3487e0def8b2711dd2a8dcc1eb1bc
Craig Burke <craig@craigburke.com>
2014-07-30 16:00:22 -0400

    More angular code cleanup

--- grails-app/assets/javascripts/angular-grails/services/crud.js
+++ grails-app/assets/javascripts/angular-grails/services/crud.js
@@ -1,6 +1,8 @@
 'use strict';
 
 function CrudService($resource, $q, $http, rootUrl, restUrl) {
+    var CrudService = {};
+
     var baseUrl = rootUrl + restUrl;
 
     var resource = $resource(baseUrl + '/:id', {id: '@id'} ,
@@ -22,8 +24,7 @@ function CrudService($resource, $q, $http, rootUrl, restUrl) {
         return promise;
     };
 
-    return {
-        list: function(params, successFn, errorFn) {
+    CrudService.list = function(params, successFn, errorFn) {
             var deferred = $q.defer();
 
             resource.query(params, function(items, headers) {
@@ -33,11 +34,14 @@ function CrudService($resource, $q, $http, rootUrl, restUrl) {
             });
 
             return chainPromise(deferred.promise, successFn, errorFn);
-        },
-        get: function(id, successFn, errorFn) {
-            return getResourcePromise(resource.get({id: id}, successFn, errorFn));
-        },
-        create: function(successFn, errorFn) {
+    };
+
+
+    CrudService.get = function(id, successFn, errorFn) {
+        return getResourcePromise(resource.get({id: id}, successFn, errorFn));
+    };
+
+    CrudService.create = function(successFn, errorFn) {
             var deferred = $q.defer();
 
             $http.get(baseUrl + "/create").success(function(data) {
@@ -45,17 +49,21 @@ function CrudService($resource, $q, $http, rootUrl, restUrl) {
             });
 
             return chainPromise(deferred.promise, successFn, errorFn);
-        },
-        delete: function(id, successFn, errorFn) {
+    };
+
+    CrudService.delete = function(id, successFn, errorFn) {
             return getResourcePromise(resource.delete({id: id}), successFn, errorFn);
-        },
-        save: function(data, successFn, errorFn) {
+    };
+
+    CrudService.save = function(data, successFn, errorFn) {
             return getResourcePromise(resource.save(data), successFn, errorFn);
-        },
-        update: function(data, successFn, errorFn) {
+    };
+
+    CrudService.update = function(data, successFn, errorFn) {
             return getResourcePromise(resource.update(data), successFn, errorFn);
-        }
-    }
+    };
+
+    return CrudService;
 }
 
 angular.module('angularGrails.services.crud', ['angularGrails.constants'])
--- grails-app/assets/javascripts/angular-grails/services/flash.js
+++ grails-app/assets/javascripts/angular-grails/services/flash.js
@@ -1,6 +1,8 @@
 'use strict';
 
 function FlashService($rootScope) {
+    var FlashService = {};
+
     var MESSAGE_TYPE = {
         ERROR: 'error',
         SUCCESS: 'success',
@@ -28,36 +30,43 @@ function FlashService($rootScope) {
         return (typeof value === 'undefined') ? defaultValue : value;
     };
 
-    return {
-        TYPES: MESSAGE_TYPE,
-        error: function(message, title) {
-            title = getDefault(title, "Error");
-            setMessage(message, title, MESSAGE_TYPE.ERROR);
-        },
-        success: function(message, title) {
-            title = getDefault(title, "Success");
-            setMessage(message, title, MESSAGE_TYPE.SUCCESS);
-        },
-        info: function(message, title) {
-            title = getDefault(title, "Info");
-            setMessage(message, title, MESSAGE_TYPE.INFO);
-        },
-        warn: function(message, title) {
-            title = getDefault(title, "Warning");
-            setMessage(message, title, MESSAGE_TYPE.WARN);
-        },
-        set: function(message, title, type) {
-            title = getDefault(title, "Info");
-            type = getDefault(type, MESSAGE_TYPE.INFO);
-            setMessage(message, title, type);
-        },
-        get: function() {
-            return _message;
-        },
-        clear: function() {
-            clearMessage();
-        }
-    }
+    FlashService.TYPES = MESSAGE_TYPE;
+
+    FlashService.error = function(message, title) {
+        title = getDefault(title, "Error");
+        setMessage(message, title, MESSAGE_TYPE.ERROR);
+    };
+
+    FlashService.success = function(message, title) {
+        title = getDefault(title, "Success");
+        setMessage(message, title, MESSAGE_TYPE.SUCCESS);
+    };
+
+    FlashService.info = function(message, title) {
+        title = getDefault(title, "Info");
+        setMessage(message, title, MESSAGE_TYPE.INFO);
+    };
+
+    FlashService.warn = function(message, title) {
+        title = getDefault(title, "Warning");
+        setMessage(message, title, MESSAGE_TYPE.WARN);
+    };
+
+    FlashService.set = function(message, title, type) {
+        title = getDefault(title, "Info");
+        type = getDefault(type, MESSAGE_TYPE.INFO);
+        setMessage(message, title, type);
+    };
+
+    FlashService.get = function() {
+        return _message;
+    };
+
+    FlashService.clear = function() {
+        clearMessage();
+    };
+
+    return FlashService;
 }
 
 angular.module('angularGrails.services.flash', [])
